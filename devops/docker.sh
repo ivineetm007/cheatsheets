fe7e141fe39f53741cd8643f0f5a12efec904af2
ivineetm007 <vineetmehta.mehta@gmail.com>
2021-04-05 13:16:25 +0530

    update

--- devops/docker.sh
+++ devops/docker.sh
@@ -2,7 +2,7 @@
 	
 ##############################################################################
 # DOCKER
-#
+##############################################################################
 #tag 
 #	<docker-hub-id>/<repo-name>:<image-name>
 #	ex nigelpoulton/gsd:first-ctr
@@ -10,10 +10,10 @@
 docker image ls
 docker image rm <name>               #Remove image
 docker container rm <name>           #Remove container
-docker run <image name>                  #Run container	 -d for background, -p <port in laptop>:<port in container>, -it interactive and ---name <name>
-										 # Exit to come out of interactive mode It will kill container. Press Ctrl + P + Q to avoid that
-docker start <container name>            #Start container
-docker stop <container name>             #Stop container
+docker run <image name>              #Run container	 -d for background, -p <port in laptop>:<port in container>, -it interactive and ---name <name>
+						             # Exit to come out of interactive mode It will kill container. Press Ctrl + P + Q to avoid that
+docker start <container name>        #Start container
+docker stop <container name>         #Stop container
 	
 ##############################################################################
 # DOCKER COMPOSE
