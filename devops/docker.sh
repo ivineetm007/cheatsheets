858a9a24861fae6e7cacc9257706d801dd1c9726
Vineet <36364743+ivineetm007@users.noreply.github.com>
2021-04-07 14:07:43 +0530

    Update docker.sh

--- devops/docker.sh
+++ devops/docker.sh
@@ -9,10 +9,11 @@
 ##############################################################################
 #IMAGE
 docker image ls
-docker image build                   #build image from dockerfile -t for tag
+docker image build                   #build image from dockerfile -t Name and optionally a tag in the 'name:tag' format
 docker image pull                    #Push an image
 docker image push                    #Pull an dockerfile
 docker image rm <name>               #Remove image
+docker image history <name>
 
 docker run <image name>              #Run container	 -d for background, -p <port in laptop>:<port in container>, -it interactive and ---name <name>. -w working directory
 						             # Exit to come out of interactive mode It will kill container. Press Ctrl + P + Q to avoid that
@@ -26,9 +27,15 @@ docker rm <container id>
 #Volume
 docker volume create todo-db         #create a volume named todo-db
 docker volume inspect todo-db        #Inspect a volume named todo-db
+docker volume ls                     #List all volumes
+docker volume remove todo-db         #Remove a volume named todo-db
+
+#network
+docker network create todo-app       #create a volume named todo-app
 ##############################################################################
 # DOCKER COMPOSE
 ##############################################################################
 docker-compose up
-docker-compose down
+docker-compose down                  # If you want to remove the volumes, you will need to add the --volumes flag.
+docker-compose logs                  # -f  flag “follows” the log, so will give you live output as it’s generated.
 
